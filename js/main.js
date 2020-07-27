1c8e3dd53e02d2e9725218d8ce20c547422d2d40
adyangga <angga.does7@gmail.com>
2020-07-28 03:16:42 +0700

    update 1 - project idul adha

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
-jQuery(document).ready(function ($) {
-    var siteSticky = function () {
-        $(".js-sticky-header").sticky({ topSpacing: 0 });
-    };
-    siteSticky();
+// jQuery(document).ready(function ($) {
+//     var siteSticky = function () {
+//         $(".js-sticky-header").sticky({ topSpacing: 0 });
+//     };
+//     siteSticky();
 
-    var siteScroll = function () {
-        $(window).scroll(function () {
-            var st = $(this).scrollTop();
-            if (st > 100) {
-                $('.js-sticky-header').addClass('shrink');
-            } else {
-                $('.js-sticky-header').removeClass('shrink');
-            }
-        })
-    };
-    siteScroll();
-});
+//     var siteScroll = function () {
+//         $(window).scroll(function () {
+//             var st = $(this).scrollTop();
+//             if (st > 100) {
+//                 $('.js-sticky-header').addClass('shrink');
+//             } else {
+//                 $('.js-sticky-header').removeClass('shrink');
+//             }
+//         })
+//     };
+//     siteScroll();
+// });
+
+var youtube = document.getElementById("youtube");
+var instagram = document.getElementById("instagram");
