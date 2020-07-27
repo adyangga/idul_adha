316d70c297881b8accdc1af64a993f4667e16e81
adyangga <angga.does7@gmail.com>
2020-07-28 02:19:34 +0700

    project idul adha

---
+++ js/main.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function ($) {
+    var siteSticky = function () {
+        $(".js-sticky-header").sticky({ topSpacing: 0 });
+    };
+    siteSticky();
+
+    var siteScroll = function () {
+        $(window).scroll(function () {
+            var st = $(this).scrollTop();
+            if (st > 100) {
+                $('.js-sticky-header').addClass('shrink');
+            } else {
+                $('.js-sticky-header').removeClass('shrink');
+            }
+        })
+    };
+    siteScroll();
+});
